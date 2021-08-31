3d7062d72241cb10eabeaa1a64b42a3e2120a750
RichardKT88 <kendy_t88@hotmail.com>
2021-08-31 16:25:01 -0300

    Add project files.

---
+++ IES/Data/IESDbInitializer.cs
@@ -0,0 +1,76 @@
+﻿using IES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IES.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+
+            PopulaDepartamento(context);
+            PopulaInstituicao(context);
+        }
+
+        private static void PopulaDepartamento(IESContext context)
+        {
+            if (context.Departamentos.Any())
+            {
+                return;
+            }
+
+            var departamentos = new Departamento[]
+            {
+                new Departamento {Nome="Ciência da Computação"},
+                new Departamento {Nome="Ciência de Alimentos"}
+            };
+
+            foreach (Departamento d in departamentos)
+            {
+                context.Departamentos.Add(d);
+                context.SaveChanges();
+            }
+        }
+        private static void PopulaInstituicao(IESContext context)
+        {
+            if (context.Instituicoes.Any())
+            {
+                return;
+            }
+
+            var instituicoes = new Instituicao[]
+            {
+                 new Instituicao() {
+                Nome = "UniParana",
+                Endereco = "Paraná"
+            },
+            new Instituicao() {
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao() {
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao() {
+                Nome = "UniSulgrandense",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao() {
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            },
+            };
+
+            foreach (Instituicao i in instituicoes)
+            {
+                context.Instituicoes.Add(i);
+                context.SaveChanges();
+            }
+        }
+    }
+}
